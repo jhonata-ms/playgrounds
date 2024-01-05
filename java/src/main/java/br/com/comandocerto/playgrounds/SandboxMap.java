fada46de6b62060d9a3e619f39cfc7748517f104
Jhonata Medeiros da Silva <jhonatams@gmail.com>
2024-01-05 15:25:08 -0300

    java: ajuste no exemplo de HashMap

--- java/src/main/java/br/com/comandocerto/playgrounds/SandboxMap.java
+++ java/src/main/java/br/com/comandocerto/playgrounds/SandboxMap.java
@@ -5,9 +5,9 @@ import java.util.Map;
 
 public class SandboxMap {
     public static void main(String[] args) {
-        Map map = new HashMap();
-        map.put(1,"Um");  
-        map.put(2,"Dois");  
+        Map<Integer, String> map = new HashMap<Integer, String>();
+        map.put(1,"Um");
+        map.put(2,"Dois");
         System.out.println(map.get(1));
         System.out.println(map.get(2));
     }
