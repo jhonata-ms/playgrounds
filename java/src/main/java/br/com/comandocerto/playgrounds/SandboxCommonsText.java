a7707ce6d52de92329e4f0f99807788bc5c642b8
Jhonata Medeiros da Silva <jhonatams@gmail.com>
2024-02-24 14:17:39 -0300

    java: exemplo de string replace

--- java/src/main/java/br/com/comandocerto/playgrounds/SandboxCommonsText.java
+++ java/src/main/java/br/com/comandocerto/playgrounds/SandboxCommonsText.java
@@ -5,5 +5,6 @@ import org.apache.commons.text.WordUtils;
 public class SandboxCommonsText {
     public static void main(String args[]) {
       System.out.println(WordUtils.capitalizeFully("jhonata"));
+      System.out.println("Eu sou um teste!".replace("um teste!", "o Jhonata!"));
     }
 }
